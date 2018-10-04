6176cbe7f3070a52a068715f304c105f53d58d2d
Anna Alexandra <annakoorkoff@gmail.com>
2018-10-04 23:12:20 +0300

    lesson1 task1 done lesson2 tasks 1-2 done lesson3 done

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -1,6 +1,11 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson3.task1
 
+import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import java.lang.Math.pow
+import kotlin.math.PI
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 /**
