fde2cdc3af4b20971bf7a9c7ce135a1e55fad5c0
Anna Alexandra <annakoorkoff@gmail.com>
2018-10-14 02:47:48 +0300

    all mistakes fixed

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -123,7 +123,7 @@ fun lcm(m: Int, n: Int): Int {
  */
 fun minDivisor(n: Int): Int {
     var minDiv = 0
-    for (i in 2..sqr(n)) if (n % i == 0) {
+    for (i in 2..sqrt(n.toDouble()).toInt()) if (n % i == 0) {
         minDiv = i
         break
     }
