bbce4f57a40c2d3deb57acdd01cfc12d376ce11a
Anna Alexandra <annakoorkoff@gmail.com>
2018-11-16 00:55:36 +0300

    all mistakes fixed

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -277,7 +277,8 @@ fun roman(n: Int): String {
     var number = n
     val answer = StringBuilder()
     val numPairs = listOf(Pair(1000, 'M'), Pair(900, "CM"), Pair(500, 'D'), Pair(400, "CD"), Pair(100, 'C'),
-            Pair(90, "XC"), Pair(50, 'L'), Pair(40, "XL"), Pair(10, 'X'), Pair(9, "IX"), Pair(5, 'V'), Pair(4, "IV"), Pair(1, 'I'))
+            Pair(90, "XC"), Pair(50, 'L'), Pair(40, "XL"), Pair(10, 'X'), Pair(9, "IX"), Pair(5, 'V'), Pair(4, "IV"),
+            Pair(1, 'I'))
     var i = 0
     while (number > 0) {
         if (number >= numPairs[i].first) {
